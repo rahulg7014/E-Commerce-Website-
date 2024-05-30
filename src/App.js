e5bfbd6153bc9a5e9f78937af26509d1153f9b01
Rahul Gupta <7014rahul@gmail.com>
2024-05-30 14:33:42 +0530

    file commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Products from './components/Products'
+import { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(json=>setData.json)
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar/>
+    <Products data = {data}/>
+    </>
+  )
 }
-
-export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+// import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+// import { useDispatch } from 'react-redux';
+
+
+export default function Products({data}) {
+  // const dispatch = useDispatch()
+  return (
+    <>
+    <img src="https://i.pinimg.com/originals/c7/e1/b7/c7e1b7b5753737039e1bdbda578132b8.gif" style={{height : "100%"}} alt="" />:
+    <div style={{display : "flex", flexWrap: "wrap", gap:"15px",margin:"5px"}} >
+    {data.map((allData)=>
+      <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="allData.image"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {allData.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {allData.description}
+        </Typography>
+      </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button sx={{fontSize: "15px",position: "relative", bottom: "-1", size: "medium", color: "primary"}}> Add To Cart </Button> 
+      </CardActions>
+    </Card>
+    )}
+    </div>
+  
+    </>
+  )
+}
